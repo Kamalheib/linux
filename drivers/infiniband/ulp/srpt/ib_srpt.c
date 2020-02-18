a07dbbd03a867945cfab725a80ee5c9d4eedbfdf
Kamal Heib <kamalheib1@gmail.com>
2020-02-18 11:13:46 +0200

    RDMA/srpt: Avoid print error when modify_port is not supported
    Avoid printing the following error when modify_port isn't supported.
    
    [47541.541145] ib_srpt disabling MAD processing failed.
    
    Fixes: a42d985bd5b2 ("ib_srpt: Initial SRP Target merge for v3.3-rc1")
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- drivers/infiniband/ulp/srpt/ib_srpt.c
+++ drivers/infiniband/ulp/srpt/ib_srpt.c
@@ -628,12 +628,14 @@ static void srpt_unregister_mad_agent(struct srpt_device *sdev)
 		.clr_port_cap_mask = IB_PORT_DEVICE_MGMT_SUP,
 	};
 	struct srpt_port *sport;
+	int ret;
 	int i;
 
 	for (i = 1; i <= sdev->device->phys_port_cnt; i++) {
 		sport = &sdev->port[i - 1];
 		WARN_ON(sport->port != i);
-		if (ib_modify_port(sdev->device, i, 0, &port_modify) < 0)
+		ret = ib_modify_port(sdev->device, i, 0, &port_modify);
+		if (ret < 0 && ret != -EOPNOTSUPP)
 			pr_err("disabling MAD processing failed.\n");
 		if (sport->mad_agent) {
 			ib_unregister_mad_agent(sport->mad_agent);
